f8c0b0a3b97823a1222a33cae8cf5748750c7f59
Dang Tu <tu07122001@gmail.com>
2023-03-09 14:21:33 +0700

    add store sticker

--- components/MyPickerComponent.tsx
+++ components/MyPickerComponent.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { PickerComponent } from 'stipop-react-sdk'
+import { PickerComponent, StoreComponent } from 'stipop-react-sdk'
 import { auth, db } from '../config/firebase'
 import { addDoc, collection, doc, serverTimestamp, setDoc } from 'firebase/firestore'
 
 const MyPickerComponent = (props: {scrollToBottom: () => void, conversationId:string | string[] | undefined}) => {
 
   const [loggedInUser, _loading, _error] = useAuthState(auth)  
+
+  const [showStore, setShowStore] = useState(false)
 
   const addStickerToDbAndUpdateLassSeen = async (sticker: any) => {
     // update last seen in 'users' collection
@@ -30,16 +32,32 @@ const MyPickerComponent = (props: {scrollToBottom: () => void, conversationId:st
 }
 
   return (
+    showStore ? 
+    <StoreComponent
+    params={{
+      apikey: '80d536bac9feef6570920372556fe027',
+      userId: loggedInUser?.uid as string,
+    }}
+    downloadParams={{
+      isPurchase: 'N'
+    }}
+    onClose={() => setShowStore(false)}
+    />  
+    :  
     <PickerComponent
-        params={{
-            apikey: '80d536bac9feef6570920372556fe027',
-            userId: loggedInUser?.uid as string,
-        }}
-        stickerClick={(url: any) => {
-            addStickerToDbAndUpdateLassSeen(url.url)
-        }}
-        storeClick={(e:any) => console.log(e)}
+      params={{
+          apikey: '80d536bac9feef6570920372556fe027',
+          userId: loggedInUser?.uid as string,
+      }}
+      stickerClick={(url: any) => {
+          addStickerToDbAndUpdateLassSeen(url.url)
+      }}
+      storeClick={(e:any) => {
+        console.log(e)
+          setShowStore(true)
+      }}
     />
+      
   )
 }
 
